28ecb43d69cb5eed3cc17d1ccba5f70f7160f873
David Vargas Hernandez <david-0610@live.com.mx>
2023-04-27 18:50:33 -0600

    install and config nativewind

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,19 +1,10 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text } from "react-native";
 import React from "react";
 
 export default function Home() {
   return (
-    <View style={styles.container}>
+    <View className="flex-1 items-center justify-center bg-white">
       <Text>Home</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "start",
-    justifyContent: "start",
-  },
-});
