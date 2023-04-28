d75ae1396cc3f90eccc85056a23b26f0bc88f392
David Vargas Hernandez <david-0610@live.com.mx>
2023-04-28 00:32:07 -0600

    add button component to reuse

---
+++ src/components/Button.js
@@ -0,0 +1,30 @@
+import { Pressable, StyleSheet, Text } from "react-native";
+import React from "react";
+
+export default function ButtonComponent(props) {
+  const { label, onPress } = props;
+  return (
+    <Pressable style={styles.button} onPress={onPress}>
+      <Text style={styles.text}>{label}</Text>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: "#324FFA",
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: "bold",
+    letterSpacing: 0.25,
+    color: "white",
+  },
+});
